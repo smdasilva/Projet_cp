a8369a7e5b2668234d8f03d914921e4dd5e23ea3
Alexandre Perrot <alexandre.perrot@gmail.com>
2012-09-25 20:50:00 +0200

    Dicom Parser
    Creating Dicom Parser class

---
+++ src/util/parser/DicomParser.java
@@ -0,0 +1,25 @@
+package util.parser;
+
+import java.util.List;
+
+public class DicomParser implements Parser {
+
+    @Override
+    public String getExtension() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public void loadFile(String filename, Examen exam) {
+	// TODO Auto-generated method stub
+	
+    }
+
+    @Override
+    public boolean saveFile(String filename, Examen exam, List<Boolean> options) {
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+}
