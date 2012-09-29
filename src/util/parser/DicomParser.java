1b2249259b9407060235d601c0bce674466c92fd
Alexandre Perrot <alexandre.perrot@gmail.com>
2012-09-29 21:16:41 +0200

    Adding Dicom reading from file

--- src/util/parser/DicomParser.java
+++ src/util/parser/DicomParser.java
@@ -1,6 +1,10 @@
 package util.parser;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import org.dcm4che2.data.DicomObject;
+import org.dcm4che2.io.DicomInputStream;
 
 public class DicomParser implements Parser {
 
@@ -17,8 +21,29 @@ public class DicomParser implements Parser {
 
     @Override
     public void loadFile(String filename, Examen exam) {
-	// TODO Auto-generated method stub
+	// reading dicom object from file
+	DicomObject dicom;
+	DicomInputStream stream = null;
+	try {
+	    stream = new DicomInputStream(new File(filename));
+	    dicom = stream.readDicomObject();
+	} catch (IOException io) {
+	    return;
+	} finally {
+	    try {
+	        stream.close();
+	    }
+	    catch (IOException ignore) {
+	    }
+	}
 	
+	boolean firstFlag = false;
+	
+	// reading informations
+	Information generalInfos = exam.getInformations();
+	if (generalInfos.getSize() == 0) {
+	    firstFlag = true;
+	}
     }
 
     /** Not supported */
