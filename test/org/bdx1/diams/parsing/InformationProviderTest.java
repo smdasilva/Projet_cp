f098e3a7a16cfa8282aa556b4719df842dbf6b45
Alexandre Perrot <alexandre.perrot@gmail.com>
2012-10-18 14:07:27 +0200

    Patient Infos parsing coded and tested

--- test/org/bdx1/diams/parsing/InformationProviderTest.java
+++ test/org/bdx1/diams/parsing/InformationProviderTest.java
@@ -3,10 +3,9 @@ package org.bdx1.diams.parsing;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Map;
 
-import org.bdx1.diams.parsing.DicomInfosProvider;
-import org.bdx1.diams.parsing.InformationProvider;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -15,11 +14,20 @@ import org.junit.Test;
 
 public class InformationProviderTest {
 
-    private static final File dcmFile = new File("resources/1.3.12.2.1107.5.1.4.50152.4.0.9805535011461839 copie.dcm");
+    private static final Map<String,String> expectedPatientInfos = new HashMap();
+    
+    private static final File dcmFile = new File("test/resources/1.3.12.2.1107.5.1.4.50152.4.0.9805533212151202 copie.dcm");
     private InformationProvider provider;
     
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
+        expectedPatientInfos.put(DicomTags.PatientName.getName(), "FRETILLERE^MARC");
+        expectedPatientInfos.put(DicomTags.PatientBirthDate.getName(), "19421029");
+        expectedPatientInfos.put(DicomTags.PatientAge.getName(), "060Y");
+        expectedPatientInfos.put(DicomTags.PatientID.getName(), "03601135");
+        expectedPatientInfos.put(DicomTags.PatientSex.getName(), "M");
+        expectedPatientInfos.put(DicomTags.PatientSize.getName(), "null");
+        expectedPatientInfos.put(DicomTags.PatientWeight.getName(), "null");
     }
 
     @AfterClass
@@ -28,7 +36,7 @@ public class InformationProviderTest {
 
     @Before
     public void setUp() throws Exception {
-        provider = new DicomInfosProvider(dcmFile);
+        provider = new DicomInfosProvider();
     }
 
     @After
@@ -38,8 +46,11 @@ public class InformationProviderTest {
 
     @Test
     public void testPatientInfos() {
+        assertTrue(provider.read(dcmFile));
         Map<String, String> infos = provider.getPatientInfos();
-        assertNotNull(infos);
+        for (String key : expectedPatientInfos.keySet()) {
+            assertEquals(expectedPatientInfos.get(key), infos.get(key));
+        }
     }
 
 }
