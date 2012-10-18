84f8a9181ca227b58f6c215cb7f1d042791c2b87
Alexandre Perrot <alexandre.perrot@gmail.com>
2012-10-18 15:10:27 +0200

    SliceInfos coded and tested

--- test/org/bdx1/diams/parsing/InformationProviderTest.java
+++ test/org/bdx1/diams/parsing/InformationProviderTest.java
@@ -15,6 +15,7 @@ import org.junit.Test;
 public class InformationProviderTest {
 
     private static final Map<String,String> expectedPatientInfos = new HashMap();
+    private static final Map<String,String> expectedSliceInfos = new HashMap();
     
     private static final File dcmFile = new File("test/resources/1.3.12.2.1107.5.1.4.50152.4.0.9805533212151202 copie.dcm");
     private InformationProvider provider;
@@ -28,6 +29,16 @@ public class InformationProviderTest {
         expectedPatientInfos.put(DicomTags.PatientSex.getName(), "M");
         expectedPatientInfos.put(DicomTags.PatientSize.getName(), "null");
         expectedPatientInfos.put(DicomTags.PatientWeight.getName(), "null");
+        
+        expectedSliceInfos.put(DicomTags.BitsAllocated.getName(), "16");
+        expectedSliceInfos.put(DicomTags.BitsStored.getName(), "12");
+        expectedSliceInfos.put(DicomTags.HighBit.getName(), "11");
+        expectedSliceInfos.put(DicomTags.RescaleIntercept.getName(), "-1024");
+        expectedSliceInfos.put(DicomTags.RescaleSlope.getName(), "1");
+        expectedSliceInfos.put(DicomTags.SliceThickness.getName(), "1");
+        expectedSliceInfos.put(DicomTags.WindowCenter.getName(), "50");
+        expectedSliceInfos.put(DicomTags.WindowCenterWidthExplanation.getName(), "WINDOW1");
+        expectedSliceInfos.put(DicomTags.WindowWidth.getName(), "350");
     }
 
     @AfterClass
@@ -44,6 +55,15 @@ public class InformationProviderTest {
         provider = null;
     }
 
+    @Test
+    public void testSliceInfos() {
+        assertTrue(provider.read(dcmFile));
+        Map<String,String> map = provider.getSliceInfos();
+        for (String key : expectedSliceInfos.keySet()) {
+            assertEquals(expectedSliceInfos.get(key), map.get(key));
+        }
+    }
+    
     @Test
     public void testPatientInfos() {
         assertTrue(provider.read(dcmFile));
