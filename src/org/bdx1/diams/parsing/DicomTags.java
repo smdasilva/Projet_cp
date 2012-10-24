15216af3aa596233efb4b77a52a898732cdba520
Alexandre Perrot <alexandre.perrot@gmail.com>
2012-10-24 18:31:10 +0200

    Changing parsing to use pixelmed again (now working /o/ )

--- src/org/bdx1/diams/parsing/DicomTags.java
+++ src/org/bdx1/diams/parsing/DicomTags.java
@@ -1,40 +1,44 @@
 package org.bdx1.diams.parsing;
 
+import com.pixelmed.dicom.AttributeTag;
+import com.pixelmed.dicom.TagFromName;
+
 
 enum DicomTags {
-    PatientName("Patient Name"),
-    PatientID("Patient ID"),
-    PatientBirthDate("Patient birthday"),
-    PatientAge("Patient age"),
-    PatientSex("Patient sex"),
-    PatientSize("Patient size"),
-    PatientWeight("Patient weight"),
-    ImagePositionPatient("Image position patient"),
-    ImageOrientationPatient("Image orientation patient"),
-    StudyID("Study data"),
-    StudyDate("Study date"),
-    StudyTime("Study time"),
-    StudyDescription("Study description"),
-    BitsAllocated("Bits allocated"),
-    BitsStored("Bits stored"),
-    HighBit("High bit"),
-    RescaleIntercept("Intercept"),
-    RescaleSlope("Slope"),
-    WindowCenter("Window center"),
-    WindowWidth("Window width"),
-    SliceThickness("Slice thickness"),
-    WindowCenterWidthExplanation("WindowCenterWidthExplanation");
+    PatientName("Patient Name",TagFromName.PatientName),
+    PatientID("Patient ID", TagFromName.PatientID),
+    PatientBirthDate("Patient birthday", TagFromName.PatientBirthDate),
+    PatientAge("Patient age", TagFromName.PatientAge),
+    PatientSex("Patient sex", TagFromName.PatientSex),
+    PatientSize("Patient size", TagFromName.PatientSize),
+    PatientWeight("Patient weight", TagFromName.PatientWeight),
+    ImagePositionPatient("Image position patient", TagFromName.ImagePositionPatient),
+    ImageOrientationPatient("Image orientation patient", TagFromName.ImageOrientationPatient),
+    StudyID("Study data", TagFromName.StudyID),
+    StudyDate("Study date", TagFromName.StudyDate),
+    StudyTime("Study time", TagFromName.StudyTime),
+    StudyDescription("Study description", TagFromName.StudyDescription),
+    BitsAllocated("Bits allocated", TagFromName.BitsAllocated),
+    BitsStored("Bits stored", TagFromName.BitsStored),
+    HighBit("High bit", TagFromName.HighBit),
+    RescaleIntercept("Intercept", TagFromName.RescaleIntercept),
+    RescaleSlope("Slope", TagFromName.RescaleSlope),
+    WindowCenter("Window center", TagFromName.WindowCenter),
+    WindowWidth("Window width", TagFromName.WindowWidth),
+    SliceThickness("Slice thickness", TagFromName.SliceThickness),
+    WindowCenterWidthExplanation("WindowCenterWidthExplanation", TagFromName.WindowCenterWidthExplanation);
 
-//    private final AttributeTag tag;
+    private final AttributeTag tag;
     private final String name;
 
-    DicomTags(String name) {
+    DicomTags(String name, AttributeTag tag) {
         this.name = name;
+        this.tag = tag;
     }
 
-//    AttributeTag getTag() {
-//        return tag;
-//    }
+    AttributeTag getTag() {
+        return tag;
+    }
 
     String getName() {
         return name;
