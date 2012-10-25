ba8804a953112f063137e0533a848fc7cd23f59d
Alexandre Perrot <alexandre.perrot@gmail.com>
2012-10-25 22:13:43 +0200

    DiamsApplication now contains current Examen

--- src/org/bdx1/diams/DiamsApplication.java
+++ src/org/bdx1/diams/DiamsApplication.java
@@ -1,7 +1,19 @@
 package org.bdx1.diams;
 
+import org.bdx1.diams.model.Examen;
+
 import android.app.Application;
 
 public class DiamsApplication extends Application {
 
+    private Examen currentExamen;
+    
+    public Examen getCurrentExamen() {
+        return this.currentExamen;
+    }
+    
+    public void setCurrentExamen(Examen newExamen) {
+        this.currentExamen = newExamen;
+    }
+    
 }
