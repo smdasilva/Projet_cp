de60bd85e37df18e55a67c37369a28e1055a75e9
Alexandre Perrot <alexandre.perrot@gmail.com>
2012-10-30 19:48:05 +0100

    Upating Caching, infos view and tests

--- src/org/bdx1/diams/caching/CachingSliceManager.java
+++ src/org/bdx1/diams/caching/CachingSliceManager.java
@@ -7,9 +7,16 @@ import java.util.List;
 import org.bdx1.diams.model.Slice;
 import org.bdx1.diams.model.SliceManager;
 
+/**
+ * This class speeds up slice handling by caching
+ * slices likely to be asked soon.
+ * @author Alexandre Perrot
+ *
+ */
 public class CachingSliceManager implements SliceManager {
 
     private final static int MAX_CACHE_SIZE = 5;
+    private final static int CACHE_REACH = 2;
     
     private List<File> files = new LinkedList<File>();
     private SliceCacher cache = new SliceCacher(MAX_CACHE_SIZE);
@@ -19,6 +26,12 @@ public class CachingSliceManager implements SliceManager {
     }
 
     public Slice getSlice(int i) {
+        for (int n=0 ; n<=CACHE_REACH ; n++) {
+            if (i+n < files.size())
+                cache.chargeInCache(files.get(i+n));
+            if (i-n >= 0)
+                cache.chargeInCache(files.get(i-n));
+        }
         return cache.getSlice(files.get(i));
     }
 
