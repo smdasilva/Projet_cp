7409283da3b0058da7fcaff5682b1cb251a5c933
redaLyazidi <reda.lyazidiProjet@gmail.com>
2012-10-31 14:57:28 +0100

    Rajout du test du Mask

---
+++ test/org/bdx1/diams/model/MaskTest.java
@@ -0,0 +1,50 @@
+package org.bdx1.diams.model;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class MaskTest {
+
+	private Mask mask;
+	private static final int nbloop = 100;
+	private static final int width = 1280;
+	private static final int height = 800;
+	private boolean[][] allfalse;
+	private boolean[][] alltrue;
+
+	@Before
+	public void setUp() throws Exception {
+		mask = new InfiniteMask();
+		allfalse = new boolean[width][height];
+		alltrue = new boolean[width][height];
+		for(int i = 0 ; i < width ; i++)
+			for(int j = 0 ; j < height ; j++)
+				alltrue[i][j] = true;
+	}
+
+	@Test
+	public void testSetPixel() {
+		InfiniteMask maskTrue = new InfiniteMask(alltrue);
+		InfiniteMask maskFalse = new InfiniteMask(allfalse);
+		assertTrue(maskTrue.getMask().size()  == width * height);
+		assertTrue(maskFalse.getMask().size() == 0);
+	}
+
+	@Test
+	public void testGetPixel() { 
+		for(int i = 0 ; i < nbloop ; i++)
+			for(int j = 0 ; j < nbloop ; j++) {
+				assertFalse(mask.getPixel(i, j));
+				mask.setPixel(i, j, true);
+				assertTrue(mask.getPixel(i, j));
+				mask.setPixel(i, j, false);
+				assertFalse(mask.getPixel(i, j));
+			}
+	}
+
+}
