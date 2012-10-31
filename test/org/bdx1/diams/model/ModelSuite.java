a213064eae948dbfa0f4d48ceaeee0a5bb0ddf22
Alexandre Perrot <alexandre.perrot@gmail.com>
2012-10-31 12:20:22 +0100

    Adding test suites

---
+++ test/org/bdx1/diams/model/ModelSuite.java
@@ -0,0 +1,11 @@
+package org.bdx1.diams.model;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ExamenTest.class, SliceTest.class })
+public class ModelSuite {
+
+}
