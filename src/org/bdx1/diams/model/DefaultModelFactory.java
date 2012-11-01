e733794c481813124ec31cf549417daa839b443f
Alexandre Perrot <alexandre.perrot@gmail.com>
2012-11-01 21:59:48 +0100

    Adding basic image adapter and test

--- src/org/bdx1/diams/model/DefaultModelFactory.java
+++ src/org/bdx1/diams/model/DefaultModelFactory.java
@@ -1,6 +1,13 @@
 package org.bdx1.diams.model;
 
+import java.io.EOFException;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.bdx1.dicom.DICOMException;
+import org.bdx1.dicom.data.DICOMImage;
+import org.bdx1.dicom.file.DICOMImageReader;
 
 /**
  * Default implementation for ModelFactory.
@@ -14,5 +21,25 @@ public class DefaultModelFactory implements ModelFactory {
     
     public Examen makeExamen(File studyFile) {
         return new Examen(studyFile, new ListSliceManager());
+    }
+
+    public static Image makeImage(File dicom) {
+        DICOMImageReader reader;
+        try {
+            reader = new DICOMImageReader(dicom);
+        } catch (FileNotFoundException e) {
+            return null;
+        }
+        DICOMImage img;
+        try {
+            img = reader.parse();
+        } catch (EOFException e) {
+            return null;
+        } catch (IOException e) {
+            return null;
+        } catch (DICOMException e) {
+            return null;
+        }
+        return new LisaImageAdapter(img.getImage());
     }
 }
