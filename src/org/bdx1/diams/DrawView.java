2b51748dcd1dc03832e47a1452e50bb24bc29fe4
smdasilva <smdasilva@wanadoo.fr>
2012-11-12 23:19:18 +0100

    view

--- src/org/bdx1/diams/DrawView.java
+++ src/org/bdx1/diams/DrawView.java
@@ -1,9 +1,11 @@
 package org.bdx1.diams;
 
+import org.bdx1.diams.model.Drawing;
 import org.bdx1.diams.model.Mask;
 import org.bdx1.diams.model.InfiniteMask;
 import org.bdx1.diams.util.CantorPair;
 import org.bdx1.diams.util.Pair;
+import org.bdx1.diams.util.Point2D;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +31,9 @@ private Path    mPath;
 private Paint       mPaint;   
 private ArrayList<Path> paths = new ArrayList<Path>();
 private Mask mask = new InfiniteMask();
+private ArrayList<Point2D> points = new ArrayList<Point2D>();
+private Drawing drawing = new Drawing(mask);
+float[][] controlPoints;
 
 public DrawView(Context context) {
     super(context);
@@ -58,50 +63,109 @@ public DrawView(Context context) {
     @Override
     protected void onDraw(Canvas canvas) {            
 
-        for (Path p : paths){
+        /*for (Path p : paths){
             canvas.drawPath(p, mPaint);
+        }*/
+        
+    	Bezier b = new Bezier();
+        float[][] controlPoints = new float [points.size()][2];
+        float[][] resultat;
+        
+        if(points.size() > 0) {
+	    	float dx = points.get(points.size()-1).x - points.get(0).x;
+	    	float dy = points.get(points.size()-1).y - points.get(0).y;
+	    	int distance = (int) Math.sqrt(Math.pow(dx,2)+ Math.pow(dy,2)); 
+	        
+	    	for (int i=0; i<points.size(); i++) {
+	        	controlPoints[i][0] = points.get(i).x;
+	        	controlPoints[i][1] = points.get(i).y;
+	        }
+	        
+	        resultat = b.computeBezierPoints(controlPoints, distance);
+	    	
+	        for(int i = 0; i< resultat.length; i++) {
+				float x = resultat[i][0];
+				float y = resultat[i][1];
+				Point2D pc = new Point2D(x,y);
+				drawing.draw(pc);
+			}
+	        
+	        Pair<Integer, Integer> paire = new Pair<Integer, Integer>(0,0);
+	        for (Integer p : mask.getMask()){
+	        	paire = CantorPair.decode(p);
+	        	int x = paire.getFirst();
+	        	int y = paire.getSecond();
+	        	canvas.drawPoint(x, y, mPaint);
+	            //System.out.println("("+ x +"," +y+ ")");  
+	        }
         }
         
-        /*
-        Pair<Integer, Integer> paire = new Pair<Integer, Integer>(0,0);
-        for (Integer p : mask.getMask()){
-        	paire = CantorPair.decode(p);
-        	int x = paire.getFirst();
-        	int y = paire.getSecond();
-        	canvas.drawPoint(x, y, mPaint);
-            //System.out.println("("+ x +"," +y+ ")");
-        }*/
+        
     }
 
     private float mX, mY;
-    private static final float TOUCH_TOLERANCE = 4;
+    private static final float TOUCH_TOLERANCE = 1;
 
     private void touch_start(float x, float y) {
-    	mPath.reset();
-        mPath.moveTo(x, y);
-        mask.setPixel((int)x, (int)y, true);
-        //mCanvas.drawPoint(x, y, mPaint);
+    	Point2D p = new Point2D(x,y);
+    	drawing.draw(p);
+    	points.add(p);
         mX = x;
         mY = y;
     }
     private void touch_move(float x, float y) {
-    	mask.setPixel((int)x, (int)y, true);
+    	Point2D p = new Point2D(x,y);
+    	drawing.draw(p);
+    	points.add(p);
+    	
     	float dx = Math.abs(x - mX);
         float dy = Math.abs(y - mY);
         if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
-            mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
+            //int distance = (int) Math.sqrt(Math.pow(dx,2)+ Math.pow(dy,2)); 
+        	//Bezier b = new Bezier();
+            //float[][] resultat;
+
+    		/*
+    		int nb = points.size();
+    		Point2D p1 = points.get(nb-1);
+    		Point2D p2 = points.get(nb-2);
+    		Point2D p3 = points.get(nb-3);
+    		
+    		controlPoints[0][0] = p1.x;
+    		controlPoints[0][1] = p1.y;
+    		controlPoints[1][0] = p2.x;
+    		controlPoints[1][1] = p2.y;
+    		controlPoints[2][0] = p3.x;
+    		controlPoints[2][1] = p3.y;
+    		*/
+    		
+    		//resultat = b.computeBezierPoints(controlPoints, distance);
+    		
+    		/*for(int i = 0; i<=distance; i++) {
+    			x = resultat[i][0];
+    			y = resultat[i][1];
+    			Point2D pc = new Point2D(x,y);
+    			points.add(pc);
+    			drawing.draw(pc);
+    			System.out.println("("+x+")"+","+y+")");
+    		}
+         */
+        	//mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
             mX = x;
             mY = y;
             //mask.setPixel((int)x, (int)y, true);
         }
     }
     private void touch_up() {
-    	mPath.lineTo(mX, mY);
+    	//mPath.lineTo(mX, mY);
         // commit the path to our offscreen
-        mCanvas.drawPath(mPath, mPaint);
-        // kill this so we don't double draw            
-        mPath = new Path();
-        paths.add(mPath);
+        //mCanvas.drawPath(mPath, mPaint);
+        //// kill this so we don't double draw            
+        //mPath = new Path();
+        //paths.add(mPath);
+    	controlPoints = new float [1][2];
+    
+    	
     }
 
 
@@ -112,7 +176,6 @@ public boolean onTouch(View arg0, MotionEvent event) {
 
       switch (event.getAction()) {
           case MotionEvent.ACTION_DOWN:
-        	  //System.out.println("down");
               touch_start(x, y);
         	  invalidate();
               break;
